a03f663ae7a586e82e78325901d0fd490eb8abb2
Akshay Raut <Akshay.Raut@gep.com>
2021-05-09 02:32:07 +0530

    Restaurants main page is working with search

--- src/App.js
+++ src/App.js
@@ -21,20 +21,20 @@ function App() {
 
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Yelp MERN</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Yelp MERN</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <Link to={"/restaurants"} className="nav-link">
                   Restaurants
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 {
                   user ? (
                     <a onClick={logout} className="nav-link" style={{ cursor: 'pointer' }}>
